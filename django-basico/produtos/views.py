5025c8d94a21bdfa60b75d05c30c2af5df1e6b20
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-07-05 16:38:48 -0300

    Update views.py

--- django-basico/produtos/views.py
+++ django-basico/produtos/views.py
@@ -5,3 +5,14 @@ def pagina_produtos(request):
 
 def celulares(request):
     return render(request, 'produtos/celulares.html')
+
+    context = {
+        'nome' : 'Matheus',
+        'ultimo acesso' : data_atual(),
+        'idade': 18,
+        'produtos': [
+            {'nome' : 'Leptop Dell', 'preco' : '1.200,00'},
+            {'nome' : 'Iphone', 'preco' : '6.400,00'},
+            {'nome' : 'Samsung', 'preco' : '2.100,00'},
+        ]
+    }
